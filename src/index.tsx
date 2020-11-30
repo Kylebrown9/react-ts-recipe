a697a5f3f0ea0294b30b3d5cad628b8b2a2f780f
Kyle Brown <kylebrw@gmail.com>
2020-11-29 19:26:10 -0500

    Removed debug logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const root = document.getElementById("root");
-console.log({ root })
-ReactDOM.render(<p>Some basic text</p>, root);
+ReactDOM.render(<p>Some basic text</p>, document.getElementById("root"));
